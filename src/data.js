af1d2b3cf084cd98e2908be23d1c792d9c79d9ce
Ahmed Maged <ahmedmaged20188@gmail.com>
2023-07-16 21:08:40 +0300

    links edit

--- src/data.js
+++ src/data.js
@@ -5,26 +5,29 @@ export const projects = [
     description:
       "An entertainment providers aggregator that collects information about every entertainment location in Egypt to help people explore the hidden gems of our country",
     image: "./khroga.gif",
-    link: "https://hangout-beta.vercel.app/",
+    link: "",
   },
   {
     title: "Target Canidian Immigration Services",
     subtitle: "Wordpress",
-    description: "Target is an immigration consultancy agency that specializes in Canadian immigration services and visa applications. Established in 2004",
+    description:
+      "Target is an immigration consultancy agency that specializes in Canadian immigration services and visa applications. Established in 2004",
     image: "./Target.gif",
     link: "https://www.targetimmigration.ca/",
   },
   {
     title: "Insight Canidian business development",
     subtitle: "Wordpress",
-    description: "Insight Canadian Business Development is a strategic management and training consultancy agency that provides various professional services and world-class solutions",
+    description:
+      "Insight Canadian Business Development is a strategic management and training consultancy agency that provides various professional services and world-class solutions",
     image: "./Insight.gif",
     link: "https://insightcbd.com/",
   },
   {
     title: "Al Rawda",
     subtitle: "Wordpress",
-    description: "Al Rawda Made up of a renowned group of experts and consultants in the real estate field, Based in fifth settlement est. 2013 ................................................",
+    description:
+      "Al Rawda Made up of a renowned group of experts and consultants in the real estate field, Based in fifth settlement est. 2013 ................................................",
     image: "./AlRawda.gif",
     link: "https://realestateeg.com/",
   },
